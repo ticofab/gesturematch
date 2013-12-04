9c230475d5fc89bc62a5ca334b1f02f6d3448107
Fabio Tiriticco <fabio.europe@gmail.com>
2013-12-04 21:47:55 +0100

    yet another renaming commit

--- app/consts/json/JsonInputLabels.scala
+++ app/consts/json/JsonInputLabels.scala
@@ -10,15 +10,15 @@ object JsonInputLabels {
   val INPUT_TYPE_DELIVERY = "delivery"
 
   // input match keys
-  val MATCH_MESSAGE_CRITERIA = "criteria"
-  val MATCH_MESSAGE_APIKEY = "apiKey"
-  val MATCH_MESSAGE_APPID = "appId"
-  val MATCH_MESSAGE_LATITUDE = "latitude"
-  val MATCH_MESSAGE_LONGITUDE = "longitude"
-  val MATCH_MESSAGE_AREASTART = "areaStart"
-  val MATCH_MESSAGE_AREAEND = "areaEnd"
-  val MATCH_MESSAGE_DEVICEID = "deviceId"
-  val MATCH_MESSAGE_EQUALITYPARAM = "equalityParam"
+  val MATCH_INPUT_CRITERIA = "criteria"
+  val MATCH_INPUT_APIKEY = "apiKey"
+  val MATCH_INPUT_APPID = "appId"
+  val MATCH_INPUT_LATITUDE = "latitude"
+  val MATCH_INPUT_LONGITUDE = "longitude"
+  val MATCH_INPUT_AREASTART = "areaStart"
+  val MATCH_INPUT_AREAEND = "areaEnd"
+  val MATCH_INPUT_DEVICEID = "deviceId"
+  val MATCH_INPUT_EQUALITYPARAM = "equalityParam"
 
   // input reason
   val INPUT_REASON = "reason"
--- app/models/ClientInputMessages/ClientInputMessageMatch.scala
+++ app/models/ClientInputMessages/ClientInputMessageMatch.scala
@@ -19,15 +19,15 @@ case class ClientInputMessageMatch(criteria: String,
 object ClientInputMessageMatch {
 
   def fromJson(jsonValue: JsValue): ClientInputMessageMatch = {
-    val criteria = (jsonValue \ JsonInputLabels.MATCH_MESSAGE_CRITERIA).asOpt[String]
-    val apiKey = (jsonValue \ JsonInputLabels.MATCH_MESSAGE_APIKEY).asOpt[String]
-    val appId = (jsonValue \ JsonInputLabels.MATCH_MESSAGE_APPID).asOpt[String]
-    val latitude = (jsonValue \ JsonInputLabels.MATCH_MESSAGE_LATITUDE).asOpt[String]
-    val longitude = (jsonValue \ JsonInputLabels.MATCH_MESSAGE_LONGITUDE).asOpt[String]
-    val areaStart = (jsonValue \ JsonInputLabels.MATCH_MESSAGE_AREASTART).asOpt[String]
-    val areaEnd = (jsonValue \ JsonInputLabels.MATCH_MESSAGE_AREAEND).asOpt[String]
-    val deviceId = (jsonValue \ JsonInputLabels.MATCH_MESSAGE_DEVICEID).asOpt[String]
-    val equalityParam = (jsonValue \ JsonInputLabels.MATCH_MESSAGE_EQUALITYPARAM).asOpt[String]
+    val criteria = (jsonValue \ JsonInputLabels.MATCH_INPUT_CRITERIA).asOpt[String]
+    val apiKey = (jsonValue \ JsonInputLabels.MATCH_INPUT_APIKEY).asOpt[String]
+    val appId = (jsonValue \ JsonInputLabels.MATCH_INPUT_APPID).asOpt[String]
+    val latitude = (jsonValue \ JsonInputLabels.MATCH_INPUT_LATITUDE).asOpt[String]
+    val longitude = (jsonValue \ JsonInputLabels.MATCH_INPUT_LONGITUDE).asOpt[String]
+    val areaStart = (jsonValue \ JsonInputLabels.MATCH_INPUT_AREASTART).asOpt[String]
+    val areaEnd = (jsonValue \ JsonInputLabels.MATCH_INPUT_AREAEND).asOpt[String]
+    val deviceId = (jsonValue \ JsonInputLabels.MATCH_INPUT_DEVICEID).asOpt[String]
+    val equalityParam = (jsonValue \ JsonInputLabels.MATCH_INPUT_EQUALITYPARAM).asOpt[String]
 
     // if we got here, it means that everything is fine
     ClientInputMessageMatch(criteria.get, apiKey.get, appId.get, latitude.get.toDouble,
