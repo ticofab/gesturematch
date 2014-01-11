42ee2d6fb687c2e5e88a55958292c87ce0aef4d6
Fabio Tiriticco <fabio.europe@gmail.com>
2014-01-11 13:24:58 +0100

    hooked in to local mongoDB

--- app/controllers/ApplicationHTTP.scala
+++ app/controllers/ApplicationHTTP.scala
@@ -2,6 +2,15 @@ package controllers
 
 import play.api.mvc.{Controller, Action}
 import play.api.Logger
+import scala.concurrent.Future
+
+// implicits
+import play.api.Play.current
+import scala.concurrent.ExecutionContext.Implicits.global
+
+// mongo db plugin
+import reactivemongo.api.{DefaultDB, MongoDriver}
+import play.modules.reactivemongo.ReactiveMongoPlugin
 
 object ApplicationHTTP extends Controller {
 
@@ -10,6 +19,18 @@ object ApplicationHTTP extends Controller {
     request => {
       Logger.info(s"Alive request at ApplicationHTTP, request: $request")
       Ok("I'm alive!\n")
+    }
+  }
+
+  // db test
+  def dbTest() = Action {
+    request => {
+
+      val db: DefaultDB = ReactiveMongoPlugin.db
+      val collezioni: Future[List[String]] = db.collectionNames
+      collezioni.map(l => println(l))
+
+      Ok("DB test passed!\n")
     }
   }
 }
