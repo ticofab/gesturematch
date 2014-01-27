cbbe79d9e396113d44f9119c2ee604380f91830f
Fabio Tiriticco <fabio.europe@gmail.com>
2014-01-27 16:02:48 +0100

    update timeouts to test production values

--- app/consts/Timeouts.scala
+++ app/consts/Timeouts.scala
@@ -3,9 +3,8 @@ package consts
 import scala.concurrent.duration.DurationInt
 
 object Timeouts {
-  val maxConnectionLifetime = 1.day
-  val maxOldestRequestInterval = 50.seconds // 4.seconds
-  val maxDatabaseResponseTime = 5.seconds // 1.seconds
+  val maxDatabaseResponseTime = 1.seconds
+  val maxConnectionLifetime = 15.minutes
+  val maxOldestRequestInterval = 3.seconds
   val maxOldestRequestIntervalMillis = maxOldestRequestInterval.toMillis
-  val minIntervalBetweenSameDeviceRequestMillis = 1500.milliseconds.toMillis
 }
