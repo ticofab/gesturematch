c3714ab00b61ca731b118b691cd71a1b2211ebaa
Fabio Tiriticco <fabio.europe@gmail.com>
2014-01-28 16:00:42 +0100

    timeout to seconds

--- app/consts/Timeouts.scala
+++ app/consts/Timeouts.scala
@@ -4,7 +4,7 @@ import scala.concurrent.duration.DurationInt
 
 object Timeouts {
   val maxDatabaseResponseTime = 1.seconds
-  val maxConnectionLifetime = 15.minutes
+  val maxConnectionLifetime = 750.seconds // 15 minutes
   val maxOldestRequestInterval = 3.seconds
   val maxOldestRequestIntervalMillis = maxOldestRequestInterval.toMillis
 }
