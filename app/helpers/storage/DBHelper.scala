98824dc5ecb82faba08efa39cf1d36278afe135c
Fabio Tiriticco <fabio.europe@gmail.com>
2014-04-14 19:11:02 +0200

    month and year included in the db info stored

--- app/helpers/storage/DBHelper.scala
+++ app/helpers/storage/DBHelper.scala
@@ -10,6 +10,7 @@ import reactivemongo.core.commands.LastError
 import scala.util.{Failure, Success}
 import consts.Timeouts
 import java.util.concurrent.TimeoutException
+import java.text.SimpleDateFormat
 
 // implicits
 
@@ -19,6 +20,7 @@ import scala.concurrent.ExecutionContext.Implicits.global
 object DBHelper {
   private val db: DefaultDB = ReactiveMongoPlugin.db
   private val coll: JSONCollection = db.collection[JSONCollection]("apiKeys")
+  private val dateFormatter: SimpleDateFormat = new java.text.SimpleDateFormat("MMMyyyy")
 
   def areKeyAndIdValid(apiKey: String, appId: String): Future[Boolean] = {
     val query = Json.obj("apiKey" -> apiKey, "appIds.appId" -> appId)
@@ -27,8 +29,10 @@ object DBHelper {
     fu.map(_.length > 0)
   }
 
+  private def getMonthYear: String = dateFormatter.format(new java.util.Date())
+
   private def updateOp(apiKey: String, appId: String, modifier: JsObject) = {
-    val query = Json.obj("apiKey" -> apiKey, "appIds.appId" -> appId)
+    val query = Json.obj("apiKey" -> apiKey, s"appIds.appId" -> appId)
     val fut: Future[LastError] = coll.update(query, modifier)
     fut.onComplete {
       case Success(le) => if (!le.ok) Logger.debug(s"DB op failed, modifier: $modifier")
@@ -37,25 +41,29 @@ object DBHelper {
   }
 
   def addMatchRequest(apiKey: String, appId: String) = {
-    val modifier = Json.obj("$inc" -> Json.obj("appIds.$.matchAttempts" -> 1))
+    val monthYear = getMonthYear
+    val modifier = Json.obj("$inc" -> Json.obj(s"appIds.$$.$monthYear.matchAttempts" -> 1))
     Logger.debug(s"add connection request, modifier: $modifier")
     updateOp(apiKey, appId, modifier)
   }
 
   def addMatchEstablished(apiKey: String, appId: String, nrMatches: Int) = {
-    val modifier = Json.obj("$inc" -> Json.obj("appIds.$.matchesEstablished" -> nrMatches))
+    val monthYear = getMonthYear
+    val modifier = Json.obj("$inc" -> Json.obj(s"appIds.$$.$monthYear.matchesEstablished" -> nrMatches))
     Logger.debug(s"add connection established, modifier: $modifier")
     updateOp(apiKey, appId, modifier)
   }
 
   def addPayloadDelivered(apiKey: String, appId: String, payloadBytes: Int) = {
-    val modifier = Json.obj("$inc" -> Json.obj("appIds.$.payloadDelivered" -> payloadBytes))
+    val monthYear = getMonthYear
+    val modifier = Json.obj("$inc" -> Json.obj(s"appIds.$$.$monthYear.payloadDelivered" -> payloadBytes))
     Logger.debug(s"add payload delivered, modifier: $modifier")
     updateOp(apiKey, appId, modifier)
   }
 
   def addPayloadReceived(apiKey: String, appId: String, payloadBytes: Int) = {
-    val modifier = Json.obj("$inc" -> Json.obj("appIds.$.payloadReceived" -> payloadBytes))
+    val monthYear = getMonthYear
+    val modifier = Json.obj("$inc" -> Json.obj(s"appIds.$$.$monthYear.payloadReceived" -> payloadBytes))
     Logger.debug(s"add payload sent, modifier: $modifier")
     updateOp(apiKey, appId, modifier)
   }
